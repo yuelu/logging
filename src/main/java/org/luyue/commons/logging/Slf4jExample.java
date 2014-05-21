2834ab3ec3e578415c3fa788246927ec0b447761
Jessica Lu <jlu@fierysigil.com>
2014-05-20 22:47:10 -0400

    added examples for slf4j, log4j, logback

---
+++ src/main/java/org/luyue/commons/logging/Slf4jExample.java
@@ -0,0 +1,25 @@
+package org.luyue.commons.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jExample {
+
+    public static void main(String[] args) {
+
+        // if using slf4j simple implementation, set logger level to TRACE
+        // System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
+
+        // if using slf4j Java logging implementation, set logger level to TRACE
+        // System.setProperty("java.util.logging.config.file", "<Path_to_logging.properties>");
+
+        Logger logger = LoggerFactory.getLogger(Slf4jExample.class);
+
+        final String message = "Hello logging!";
+        logger.trace(message);
+        logger.debug(message);
+        logger.info(message);
+        logger.warn(message);
+        logger.error(message);
+    }
+}
